af1480dddcc283cccf7929caa92aa7076b5e9aba
Phillip Thain <pthain@purdue.edu>
2019-05-14 15:10:26 -0400

    Add a button that creates a new window (sample 'modal.html' borrowed from electron demo)

--- renderer.js
+++ renderer.js
@@ -1,3 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+console.log("This is the renderer process!")
+const {BrowserWindow} = require('electron').remote;
+const path = require('path')
+
+//Get a button id'd by 'new-window' found in index.html
+<br>
+const newWindowBtn = document.getElementById('new-window')
+
+newWindowBtn.addEventListener('click', (event) => {
+  const modalPath = path.join('file://', __dirname,'modal.html')
+  let win = new BrowserWindow({width: 400, height: 320})
+
+  win.on('close', () => {win = null})
+  win.loadURL(modalPath)
+  win.show()
+})
