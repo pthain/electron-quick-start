3084dde26695a849f78731b30ce6e1bcd7190ccc
Phillip Thain <pthain@purdue.edu>
2019-05-19 16:04:37 -0400

    Fixed compilaiton error caused by <br> tag in renderer.js

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ const {app, BrowserWindow} = require('electron')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
+console.log("This is the main process!")
+
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({
@@ -19,13 +21,14 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
+    console.log("Closing...")
     mainWindow = null
   })
 }
--- renderer.js
+++ renderer.js
@@ -1,16 +1,19 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
 console.log("This is the renderer process!")
+
 const {BrowserWindow} = require('electron').remote;
 const path = require('path')
 
 //Get a button id'd by 'new-window' found in index.html
-<br>
 const newWindowBtn = document.getElementById('new-window')
 
 newWindowBtn.addEventListener('click', (event) => {
+  console.log("Creating a new window (because you pressed a button)")
   const modalPath = path.join('file://', __dirname,'modal.html')
+  console.log("Path: ",modalPath)
   let win = new BrowserWindow({width: 400, height: 320})
 
   win.on('close', () => {win = null})
