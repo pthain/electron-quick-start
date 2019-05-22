870fba159b1a8e6b1d77f8b91690aab12c239519
Phillip Thain <pthain@purdue.edu>
2019-05-22 00:25:15 -0400

    Implement window management. Learned about the backtick(`) Template-literal.

--- renderer.js
+++ renderer.js
@@ -6,17 +6,39 @@ console.log("This is the renderer process!")
 
 const {BrowserWindow} = require('electron').remote;
 const path = require('path')
+let win
 
 //Get a button id'd by 'new-window' found in index.html
 const newWindowBtn = document.getElementById('new-window')
+const manageWindowBtn = document.getElementById('manage-window')
 
 newWindowBtn.addEventListener('click', (event) => {
-  console.log("Creating a new window (because you pressed a button)")
-  const modalPath = path.join('file://', __dirname,'modal.html')
+  console.log("creating new window")
+  const modalPath = path.join('file://', __dirname,'html_src/modal.html')
   console.log("Path: ",modalPath)
-  let win = new BrowserWindow({width: 400, height: 320})
+  win = new BrowserWindow({width: 400, height: 320})
 
   win.on('close', () => {win = null})
   win.loadURL(modalPath)
   win.show()
+})
+
+manageWindowBtn.addEventListener('click', (event) => {
+  console.log("creating manageable window")
+  const modalPath = path.join('file://', __dirname,'html_src/manage-modal.html')
+  console.log("Path: ",modalPath)
+  win = new BrowserWindow({width: 400, height: 320})
+
+  win.on('resize', updateReply)
+  win.on('move', updateReply)
+  win.on('close', () => {win = null})
+  win.loadURL(modalPath)
+  win.show()
+
+  function updateReply() {
+    const manageWindowReply = document.getElementById('manage-window-reply')
+    const message = `Size: ${win.getSize()} Position: ${win.getPosition()}`
+    manageWindowReply.innerText = message
+  }
+
 })
