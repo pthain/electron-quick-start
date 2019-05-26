2a8dddbbd94eefb7853b35a9908d3e1c07355caa
Phillip Thain <pthain@purdue.edu>
2019-05-25 23:56:27 -0400

    Setup top menu test environment

--- renderer.js
+++ renderer.js
@@ -1,3 +1,18 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const {BrowserWindow} = require('electron').remote
+const path = require('path')
+
+const menuTestBtn = document.getElementById('menu-test-window')
+let win
+
+menuTestBtn.addEventListener('click', () => {
+  win = new BrowserWindow({width: 400, height: 320})
+  const htmlPath = path.join('file://',__dirname,'html_src/modal.html')
+
+  win.on('close', () => {win = null})
+  win.loadURL(htmlPath)
+  win.show()
+})
